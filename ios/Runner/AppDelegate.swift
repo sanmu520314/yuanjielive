9614da6665d52849d2ba616987742a7912c07394
duanqingsen <sanmu616@163.com>
2025-12-19 18:45:08 +0800

    commit ios cert

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,19 @@
 import UIKit
 import Flutter
 
+// 插入这段代码来修复阿里 SDK 的协议冲突
+protocol Launcher {
+    func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey : Any], completionHandler completion: ((Bool) -> Void)?)
+}
+
+extension UIApplication: Launcher {
+    // 阿里 SDK 要求的兼容性代码
+      func open(_ url: URL, options: [OpenExternalURLOptionsKey : Any], completionHandler completion: ((Bool) -> Void)?) {
+            self.open(url, options: options, completionHandler: completion)
+            }
+}
+
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
