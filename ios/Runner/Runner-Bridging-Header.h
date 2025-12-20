971803e66e44f46535223bcbc16ca32859dc8c5e
duanqingsen <sanmu616@163.com>
2025-12-20 14:26:35 +0800

    commit ios cert

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,8 +1,8 @@
 #ifndef Runner_Bridging_Header_h
 #define Runner_Bridging_Header_h
 
-// 此时 AlivcLivePusherPlugin.h 已经在当前目录下
 #import "GeneratedPluginRegistrant.h"
+// 因为文件就在同级目录，直接引用
 #import "AlivcLivePusherPlugin.h"
 
 #endif
