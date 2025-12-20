ac7b0ccce99e091ce4ae5661e30ab07c51335250
duanqingsen <sanmu616@163.com>
2025-12-20 14:31:37 +0800

    commit ios cert

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -2,7 +2,7 @@
 #define Runner_Bridging_Header_h
 
 #import "GeneratedPluginRegistrant.h"
-// 因为文件就在同级目录，直接引用
+// 因为 AlivcLivePusherPlugin.h 就在同级目录下，直接引用
 #import "AlivcLivePusherPlugin.h"
 
 #endif
