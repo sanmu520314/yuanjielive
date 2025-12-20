aeff40279a436ecc5cfed0af78ee862708a24f10
duanqingsen <sanmu616@163.com>
2025-12-20 13:33:06 +0800

    commit ios cert

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,2 +1,9 @@
+#ifndef Runner_Bridging_Header_h
+#define Runner_Bridging_Header_h
+
 #import "GeneratedPluginRegistrant.h"
+
+// 尝试去掉具体的插件名，直接引用。因为我们已经在 Search Paths 里加了 Classes/**
 #import "AlivcLivePusherPlugin.h"
+
+#endif
