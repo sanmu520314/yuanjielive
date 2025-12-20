1901517eee8085ad9ce8c20c0b7a1dc1b4c9afad
duanqingsen <sanmu616@163.com>
2025-12-20 13:25:12 +0800

    commit ios cert

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
 import Flutter
 import UIKit
 
-import flutter_livepush_plugin
+
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,12 +9,6 @@ import flutter_livepush_plugin
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
-    // 2. 关键：在这里手动注册插件，不完全依赖 GeneratedPluginRegistrant
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    // 如果下面这行报错，可以尝试先注释掉，看看能否编译通过
-    // AlivcLivePusherPlugin.register(with: self.registrar(forPlugin: "AlivcLivePusherPlugin")!)
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
